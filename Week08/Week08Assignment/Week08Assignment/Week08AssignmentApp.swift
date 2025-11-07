269f89aa05bae47f134310939e1bf92079d2182a
jerryisjenny <jt4993@nyu.edu>
2025-11-07 06:22:04 -0500

    firebase

---
+++ Week08/Week08Assignment/Week08Assignment/Week08AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week08AssignmentApp.swift
+//  Week08Assignment
+//
+//  Created by Jieyin Tan on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Week08AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
